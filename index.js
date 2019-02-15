f3feda48ba39bdb408ef9bb9a051303c3c1fb0ba
wunderszindi <sandrine.varga@gmail.com>
2019-02-15 15:45:02 +0100

    2d fv

---
+++ index.js
@@ -0,0 +1,14 @@
+let arrayTwoD = (a, b) => {
+  let arr = [];
+  for (let i = 0; i < a; i++) {
+    let arr2 = [];
+    for (let j = 0; j < b; j++) {
+      arr2.push(0);
+
+    } 
+    arr.push(arr2);
+  }
+  return arr;
+};
+
+console.log(arrayTwoD(5, 6));
